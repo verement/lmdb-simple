88b038cee8b356477a8d8bf5c8ff980de00d38a9
Ximin Luo <ximin@dfinity.org>
2019-06-17 14:25:03 -0700

    Don't use runInBoundThread

--- src/Database/LMDB/Simple.hs
+++ src/Database/LMDB/Simple.hs
@@ -79,8 +79,9 @@ module Database.LMDB.Simple
   , SubMode
   ) where
 
-import Control.Concurrent
-  ( runInBoundThread
+import Control.Concurrent.Async
+  ( withAsyncBound
+  , wait
   )
 
 import Control.Exception
@@ -208,10 +209,16 @@ openEnvironment path limits = do
           | Errno (fromIntegral code) == eNOTDIR = True
         isNotDirectoryError _                    = False
 
+-- | Version of @runInBoundThread@ that may be safely interrupted.
+--
+-- See https://github.com/verement/lmdb-simple/issues/6
+runInBoundThread' :: IO a -> IO a
+runInBoundThread' action = withAsyncBound action wait
+
 -- | Closes an open envrionment. After calling this function, the
 -- environment should *not* be used again.
 closeEnvironment :: Mode mode => Environment mode -> IO ()
-closeEnvironment (Env mdb_env) = runInBoundThread $ mdb_env_close mdb_env
+closeEnvironment (Env mdb_env) = runInBoundThread' $ mdb_env_close mdb_env
 
 -- | Convenience function for opening an LMDB environment in 'ReadWrite'
 -- mode; see 'openEnvironment'
@@ -259,7 +266,7 @@ transaction :: (Mode tmode, SubMode emode tmode)
             => Environment emode -> Transaction tmode a -> IO a
 transaction (Env env) tx@(Txn tf)
   | isReadOnlyTransaction tx = run True
-  | otherwise                = runInBoundThread (run False)
+  | otherwise                = runInBoundThread' (run False)
   where run readOnly =
           bracketOnError (mdb_txn_begin env Nothing readOnly) mdb_txn_abort $
           \txn -> tf txn >>= \result -> mdb_txn_commit txn >> return result
